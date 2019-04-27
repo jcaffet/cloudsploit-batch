c22c7d98f9e741a14a73f15643fa7e675cb8665c
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-04-27 12:02:55 +0200

    Little syntax refactoring in script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,15 @@ TMP_ASSUME_ROLE_FILE=/tmp/assume-role.json
 echo "Collecting credentials for ${ACCOUNT} for role ${CLOUDSPLOIT_ROLE_TO_ASSUME}"
 aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT}:role/${CLOUDSPLOIT_ROLE_TO_ASSUME} \
 										--external-id ${CLOUDSPLOIT_ROLE_EXTERNALID} \
-	                  --role-session-name ${CLOUDSPLOIT_ROLE_TO_ASSUME} >${TMP_ASSUME_ROLE_FILE}
+	                  --role-session-name ${CLOUDSPLOIT_ROLE_TO_ASSUME} \
+										>${TMP_ASSUME_ROLE_FILE}
 
 export AWS_SECRET_ACCESS_KEY=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SecretAccessKey`
 if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then echo "AWS_SECRET_ACCESS_KEY not set !"; exit 1; fi
+
 export AWS_ACCESS_KEY_ID=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
 if [ -z "${AWS_ACCESS_KEY_ID}" ]; then echo "AWS_ACCESS_KEY_ID not set !"; exit 1; fi
+
 export AWS_SESSION_TOKEN=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SessionToken`
 if [ -z "${AWS_SESSION_TOKEN}" ]; then echo "AWS_SESSION_TOKEN not set !"; exit 1; fi
 
